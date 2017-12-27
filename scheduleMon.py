a3486719a4e8fd07238d740ea2fb66ae311ee267
nunezpaul <nunez.paul@utexas.edu>
2017-12-26 22:49:25 -0800

    first commit

---
+++ scheduleMon.py
@@ -0,0 +1,9 @@
+from AutoScheduler import scheduleSkedda
+
+#This is to be executed on Tuesday 4 weeks in advance
+
+if __name__ == '__main__':
+        #schedule for Wednesdsay
+        date = scheduleSkedda(-1,
+                              title = 'WCS Dance Show Practice',
+                              body = 'West Coast Swing practice for danceshow')
---
+++ scheduleWed.py
@@ -0,0 +1,7 @@
+from AutoScheduler import scheduleSkedda
+
+#This is to be executed on Tuesday 4 weeks in advance
+
+if __name__ == '__main__':
+        #schedule for Monday
+        date = scheduleSkedda(1)
