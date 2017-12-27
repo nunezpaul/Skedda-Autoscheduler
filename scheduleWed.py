7f3efe5f46cc7b27a3dd6ac5c01193f21fee906e
Paul N <nunez.paul@utexas.edu>
2017-12-27 01:30:23 -0800

    Update scheduleWed.py

--- scheduleWed.py
+++ scheduleWed.py
@@ -3,5 +3,5 @@ from AutoScheduler import scheduleSkedda
 #This is to be executed on Tuesday 4 weeks in advance
 
 if __name__ == '__main__':
-        #schedule for Monday
+        #Schedule south cats rec room from 7-10pm on Wednesday
         date = scheduleSkedda(1)
