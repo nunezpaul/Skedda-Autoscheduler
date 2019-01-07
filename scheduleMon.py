2b508fbbf1155d6f993525155edcb02628c94b9b
nunezpaul <nunez.paul@utexas.edu>
2019-01-06 17:15:55 -0800

    removing unnecessary files

--- scheduleMon.py
+++
@@ -1,9 +0,0 @@
-from AutoScheduler import scheduleSkedda
-
-#This is to be executed on Tuesday 4 weeks in advance
-
-if __name__ == '__main__':
-        #schedule South Cats Room from 7-10pm on Monday
-        date = scheduleSkedda(-1,
-                              title = 'WCS Dance Show Practice',
-                              body = 'West Coast Swing practice for danceshow')
--- scheduleWed.py
+++
@@ -1,7 +0,0 @@
-from AutoScheduler import scheduleSkedda
-
-#This is to be executed on Tuesday 4 weeks in advance
-
-if __name__ == '__main__':
-        #Schedule south cats rec room from 7-10pm on Wednesday
-        date = scheduleSkedda(1)
