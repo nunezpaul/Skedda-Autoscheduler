5741fe41c3decdc03574e2a9d89d131193683e7f
nunezpaul <nunez.paul@utexas.edu>
2019-01-06 17:16:18 -0800

    Now able to parse elements from command line

---
+++ config.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+class Config(object):
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Parameters for training model.')
+        parser.add_argument('--username', type=str, default=None,
+                            help='Username to be used to log into Skedda.')
+        parser.add_argument('--password', type=str, default=None,
+                            help='Password to be used to log into Skedda.')
+        parser.add_argument('--title', type=str, default='West Coast Swing Dance Social',
+                            help='Title of the event that is to be posted.')
+        parser.add_argument('--body', type=str, default='Come dance the California state dance with us!',
+                            help='Body of the event that is to be posted.')
+        parser.add_argument('--num_weeks_away', type=int, default=4,
+                            help='Number of weeks from today in which the room is to be scheduled. Additive with days.')
+        parser.add_argument('--num_dayss_away', type=int, default=1,
+                            help='Number of days from today in which the room is to be scheduled. Additive with weeks.')
+        parser.add_argument('--debug', action='store_true', default=False,
+                            help='Will take screenshots of each step. Helpful for debugging.')
+        self.params = vars(parser.parse_args())
+
+
+if __name__ == '__main__':
+    config = Config()
+    print(config.params)
